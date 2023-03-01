5225711e10c124549eefaf870d010d58af3df85e
db-lyon <david@bingham.me>
2023-02-28 22:45:31 -0700

    Implemented input validation in handleSubmit()

--- force-app/main/default/lwc/pedigreeTable/pedigreeTable.js
+++ force-app/main/default/lwc/pedigreeTable/pedigreeTable.js
@@ -1,5 +1,6 @@
 import { LightningElement, track, api, wire } from 'lwc';
 import { FlowNavigationNextEvent } from 'lightning/flowSupport';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 import createHorse from '@salesforce/apex/PedigreeTableController.createHorse';
 import updateHorses from '@salesforce/apex/PedigreeTableController.updateHorses';
 
@@ -14,7 +15,8 @@ export default class PedigreeTable extends LightningElement {
     async connectedCallback() {
         console.log("LEAD ID: "+ this.leadId);
         try {
-            const data = await createHorse({ objectApiName: HORSE_OBJECT.objectApiName, leadId: this.leadId});
+            const data = await createHorse({ objectApiName: HORSE_OBJECT.objectApiName, leadId: this.leadId });
+            data.Owner__c = true;
             this.horses = [data];
             this.idIndex = 1;
             console.log("INIT HORSES: " + JSON.stringify(this.horses));
@@ -27,6 +29,7 @@ export default class PedigreeTable extends LightningElement {
         createHorse({ objectApiName: 'Horse__c', leadId: this.leadId })
             .then(newHorse => {
                 // Add the new horse to the horses array and increment the ID index
+                newHorse.Owner__c = true;
                 this.horses = [...this.horses, newHorse];
                 this.idIndex++;
 
@@ -44,24 +47,53 @@ export default class PedigreeTable extends LightningElement {
     handleSubmit() {
         const rows = this.template.querySelectorAll('c-pedigree-table-row');
         const horses = [];
+
+        // assume all fields are populated by default
+        let allFieldsPopulated = true; 
+
         rows.forEach(row => {
             let horse = row.horse;
             horses.push(horse);
+
+            // Define regular expressions for email and phone number formats
+            const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+            const phoneRegex = /^\d{10}$/;
+
+            // perform validation
+            if (!horse.Name__c || !horse.Registration_Number__c || 
+                !horse.Gender__c || !horse.Date_Foaled__c ||
+                !horse.Sire__c || !horse.Dam__c || !horse.Color__c ||
+                (horse.Other__c && !horse.Other_Breed_Association__c) ||
+                (horse.Owner__c === false && (!horse.OwnerFirstName || !horse.OwnerLastName || 
+                                              !emailRegex.test(horse.OwnerEmail) || !phoneRegex.test(horse.OwnerPhone)))
+            ) {
+                allFieldsPopulated = false;
+            }
         });
         console.log("STRINGIFIED HORSES:" + JSON.stringify(horses));
+        
+        // check if validation succeeded
+        if(allFieldsPopulated){
+            updateHorses({horses:JSON.stringify(horses)})
+            .then(result => {
+                console.log('Data:'+ JSON.stringify(result));
+            }) .catch(error => {
+                console.log(error);
+                this.error = error;
+            }); 
 
-        // SEND JSON STRINGIFIED HORSES TO updateHorses()
-        updateHorses({horses:JSON.stringify(horses)})
-        .then(result => {
-            console.log('Data:'+ JSON.stringify(result));
-        }) .catch(error => {
-            console.log(error);
-            this.error = error;
-        }); 
-
-        if (this.availableActions.find((action) => action === 'NEXT')) {
-            const navigateNextEvent = new FlowNavigationNextEvent();
-            this.dispatchEvent(navigateNextEvent);
+            if (this.availableActions.find((action) => action === 'NEXT')) {
+                const navigateNextEvent = new FlowNavigationNextEvent();
+                this.dispatchEvent(navigateNextEvent);
+            }
+        } else {
+            // show a toast message if not all required fields are populated
+            const toastEvent = new ShowToastEvent({
+                title: 'Error',
+                message: 'Please fill in all required fields for each horse.',
+                variant: 'error'
+            });
+            this.dispatchEvent(toastEvent);
         }
     }
 }
